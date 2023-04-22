521eaecd318786bee48c334957c7afbb7ad9dc47
dazzledazzleb <dazzledazzleb@gmail.com>
2023-04-22 21:16:39 +0900

    Search Bar 추가 검색 결과의 갯수를 보여줄 Label 추가

--- UnSplashAPI_CollectionView/UnSplashAPI_CollectionView/ViewController.swift
+++ UnSplashAPI_CollectionView/UnSplashAPI_CollectionView/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var SearchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
 }
 
