1c515eccedcd6234fa2cb66a7a6c3a64c06f55d5
dazzledazzleb <dazzledazzleb@gmail.com>
2023-04-22 21:33:06 +0900

    UILabel 수정 & OutLet 등록

--- UnSplashAPI_CollectionView/UnSplashAPI_CollectionView/ViewController.swift
+++ UnSplashAPI_CollectionView/UnSplashAPI_CollectionView/ViewController.swift
@@ -10,14 +10,27 @@ import UIKit
 class ViewController: UIViewController {
 
     
-    
+    //MARK: - 멤버 변수
     @IBOutlet weak var SearchBar: UISearchBar!
+    @IBOutlet weak var CountLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+//TODO: - CollectionView 생성
+    
+    
+    
+//TODO: - SearchBar 를 통해 검색 했을 때 API 를 통해 검색 결과 값 JSON을 파싱 후, CollectionView 의 각 Cell 들에게 필요 Data 를 전달하여 UI 표현
+    // 1. 검색 했을 때 API 통신
+    // 2. 데이터 파싱
+    // 3. 파싱 데이터 Cell 전달
+    // 4. Label 총 검색 결과의 갯수 를 전달
+    
+    
+    
     
 }
 
